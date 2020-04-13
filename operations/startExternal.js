9743533df3edb33968dbb63a2942bc1f00d73412
Jørgen Landsnes <jorgen.landsnes@miles.no>
2020-04-13 10:42:13 +0200

    feat: Enable filter and apply-to-all for startExternal

--- operations/startExternal.js
+++ operations/startExternal.js
@@ -1,9 +1,21 @@
 const shell = require('shelljs');
 
 module.exports = (context, container) => {
-  const config = context.external[container];
+  const run = [];
+  if (container) {
+    if (context.external[container]) {
+      run.push(Object.assign({}, context.external[container], { name: container }));
+    } else {
+      console.error(`Could not find container matching ${container}`);
+      return;
+    }
+  } else {
+    Object.keys(context.external).forEach((c) => run.push(Object.assign({}, context.external[c], { name: c })));
+  }
 
-  const cmd = `docker run -d ${config.port ? '-p ' + config.port : ''} ${config.volume ? '-v ' + context.root + '/' + config.volume : ''} --network ${context.name} --name ${container} ${config.image}`;
-  shell.echo(cmd)
-  shell.exec(cmd);
+  run.forEach((config) => {
+    const cmd = `docker run -d ${config.port ? '-p ' + config.port : ''} ${config.volume ? '-v ' + context.root + '/' + config.volume : ''} --network ${context.name} --name ${config.name} ${config.image}`;
+    shell.echo(cmd)
+    shell.exec(cmd);
+  });
 };
