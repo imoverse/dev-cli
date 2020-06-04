50e04512c19d4d1582bf6c485455526b86222d89
Jørgen Landsnes <jorgen.landsnes@miles.no>
2020-06-04 07:58:24 +0200

    feat: Add support for overriding cmd and disabling volume sharing in dev containers

--- helpers/run_container.js
+++ helpers/run_container.js
@@ -3,19 +3,30 @@ const shell = require('shelljs');
 const runContainer = (context, containerConfig) => {
   let flags = ['-d'];
   let itr = 0;
+  const {
+    name,
+    port,
+    cmd = 'npm run dev',
+    volume,
+  } = containerConfig;
   context.options.forEach(flag => {
     if (flag.startsWith("-")) { flags[itr++] = flag }
     else flags[1] = flag;
   });
-  const workingDir = `${context.root}/${containerConfig.name}`;
-  let port = containerConfig.port;
-  if (typeof containerConfig.port == 'number' || port.indexOf(':') === -1) {
-    port = `${port}:3000`;
+  const workingDir = `${context.root}/${name}`;
+  let cmdPort = port;
+  if (typeof port === 'number' || port.indexOf(':') === -1) {
+    cmdPort = `${port}:3000`;
   }
-  const cmd = `docker run ${flags[0]} -p ${port} -v ${workingDir}:/app --network ${context.name} --name ${containerConfig.name} ${containerConfig.name} npm run dev`;
+  let containerCmd = cmd === 'default' ? '' : cmd;
+  let containerVolume = `-v ${workingDir}:/app`;
+  if (volume === false) {
+    containerVolume = '';
+  }
+  const run = `docker run ${flags[0]} -p ${port} ${containerVolume} --network ${context.name} --name ${containerConfig.name} ${containerConfig.name} ${containerCmd}`;
 
-  shell.echo(cmd);
-  shell.exec(cmd);
+  shell.echo(run);
+  shell.exec(run);
 };
 
 module.exports = runContainer;
