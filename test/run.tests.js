0bb82fc366b5e26e64515e17ad49c04c6e3f3e9d
Jørgen Landsnes <jorgen.landsnes@miles.no>
2020-08-11 09:22:38 +0200

    tests: More run tests

--- test/run.tests.js
+++ test/run.tests.js
@@ -37,3 +37,41 @@ test('Should bind to container port 3000 if shorthand mapping is provided', t =>
 
   mock.stop('shelljs');
 });
+
+
+test('Should override cmd if provided', t => {
+  mock('shelljs', { 
+    exec: (cmd) => {
+      t.assert(cmd.indexOf('custom command') > -1);
+    },
+    echo: (cmd) => {
+      // noop
+    },
+  });
+  mock.reRequire('../helpers/run_container');
+  sut = mock.reRequire('../operations/run');
+  const ctx = { name: 'local-test', root: '', options: [], containers: [{ name: 'foo', port: 3210, cmd: 'custom command' }]};
+  
+  sut(ctx, containerName);
+
+  mock.stop('shelljs');
+});
+
+test('Should map env to -e flags', t => {
+  mock('shelljs', { 
+    exec: (cmd) => {
+      console.log(cmd);
+      t.assert(cmd.indexOf('-e foo=1 -e bar=2') > -1);
+    },
+    echo: (cmd) => {
+      // noop
+    },
+  });
+  mock.reRequire('../helpers/run_container');
+  sut = mock.reRequire('../operations/run');
+  const ctx = { name: 'local-test', root: '', options: [], containers: [{ name: 'foo', port: 3210, env: [{ foo: 1 }, { bar: 2}] }]};
+  
+  sut(ctx, containerName);
+
+  mock.stop('shelljs');
+});
