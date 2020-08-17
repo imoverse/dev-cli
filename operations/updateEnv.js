954b2e1d265680152c2276047350dac7850f55b2
Jørgen Landsnes <jorgen.landsnes@miles.no>
2020-08-17 08:37:27 +0200

    feat: Add IMOVE_ENV_FOLDER variable

--- operations/updateEnv.js
+++ operations/updateEnv.js
@@ -2,7 +2,7 @@ const shell = require('shelljs');
 const fs = require('fs');
 const chalk = require('chalk');
 
-module.exports = (context, envFolder = `${process.env.HOME}/Dropbox/imove-dev/env`) => {
+module.exports = (context, envFolder = process.env.IMOVE_ENV_FOLDER ? process.env.IMOVE_ENV_FOLDER : `${process.env.HOME}/Dropbox/imove-dev/env`) => {
   const containers = context.containers.map(c => c.name);
 
   fs.access(envFolder, fs.constants.R_OK, (err) => {
@@ -10,7 +10,6 @@ module.exports = (context, envFolder = `${process.env.HOME}/Dropbox/imove-dev/en
       console.log(chalk`{red Could not find env files folder at ${envFolder}. Please provide a valid path to the env file folder. }`);
       return process.exit(1);
     }
-    console.log(context.root)
     containers.forEach((c) => {
       cmd = `cp ${envFolder}/${c}.env ${context.root}/${c}/.env`;
       shell.exec(cmd);
