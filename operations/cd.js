779e4a6a976c22da7ba1ea8b7419008c50718ca8
Jørgen Landsnes <jorgen.landsnes@miles.no>
2020-08-18 10:20:25 +0200

    fix: Simplify cd using repo path

--- operations/cd.js
+++ operations/cd.js
@@ -1,8 +1,8 @@
 const { findAndApply } = require('../helpers/find');
 
 module.exports = (context, search) => {
-  const match = findAndApply(context, search, (_, repo) => {
-    return `${context.root}/${repo.name}`;
+  const match = findAndApply(context, search, (_, { path }) => {
+    return path;
   });
   console.log(match || context.root);
 };
