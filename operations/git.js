5a6736a89533ea5713d0610e6e3534c5a7838705
Jørgen Landsnes <jorgen.landsnes@miles.no>
2020-08-18 09:59:15 +0200

    feat: Refactor git functions

--- operations/git.js
+++ operations/git.js
@@ -1,50 +1,14 @@
-const { container: findContainer, package: findPackage, otherRepo: findOtherRepo } = require('../helpers/find');
+const { findAndApply } = require('../helpers/find');
+
 const shell = require('shelljs');
 
 module.exports = (context, op = 'clone', search) => {
 
-  const cloneRepo = (name, root = context.root) => {
-    const target = `${root}/${name}`;
-    const cmd = `git clone ${context.gitBasePath}/${name}.git ${target}`;
+  findAndApply(context, search, (_, { name }) => {
+    const target = `${context.root}/${name}`;
+    const cmd = op === 'clone' ? `git clone ${context.gitBasePath}/${name}.git ${target}` : `git -C ${target} pull --rebase`;
     shell.echo(cmd)
     shell.exec(cmd);
-  };
-
-  const pullRepo = (name, root = context.root) => {
-    const target = `${root}/${name}`;
-    const cmd = `git -C ${target} pull --rebase`;
-    shell.echo(cmd)
-    shell.exec(cmd);
-  };
-  const fn = op === 'clone' ? cloneRepo : pullRepo;
-
-  if (search) {
-    const container = findContainer(context, search);
-    if (container) {
-      fn(container.name);
-    } else {
-      const package = findPackage(context, search);
-      if (package) {
-        fn(package.name, `${context.root}/packages`);
-      } else {
-        const otherRepo = findOtherRepo(context, search);
-        if (otherRepo) {
-          fn(otherRepo.name);
-        }else {
-          console.error(`Could not find container matching '${search}'`);
-        }
-      }
-    }
-    return;
-  }
-
-  context.containers.forEach(({ name }) => {
-    fn(name);
   });
-  context.packages.forEach(({ name }) => {
-    fn(name, `${context.root}/packages`);
-  });
-  context.otherRepos.forEach(({ name }) => {
-    fn(name);
-  });
-}
+
+};
