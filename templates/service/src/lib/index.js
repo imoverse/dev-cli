1dc2318ea3cea41bc3bf19ec7d46cb00dc979c7d
Jørgen Landsnes <jorgen.landsnes@miles.no>
2020-08-31 13:26:58 +0200

    chore: Remove unused files

--- templates/service/src/lib/index.js
+++
@@ -1,5 +0,0 @@
-exports.corsOptions = require('./corsOptions');
-exports.runSql = require('./runSql');
-exports.validateInput = require('./validateInput');
-exports.validate = require('./validate');
-exports.publishEvent = require('./publishEvent');
