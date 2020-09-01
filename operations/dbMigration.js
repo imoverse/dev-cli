dc085350ae5929e511dbb367b7bffe6d073d44b6
Jørgen Landsnes <jorgen.landsnes@miles.no>
2020-09-01 12:30:55 +0200

    fix: Make sure imove-pg is running before dbMigration

--- operations/dbMigration.js
+++ operations/dbMigration.js
@@ -1,7 +1,9 @@
 const shell = require('shelljs');
 const { findAndApply } = require('../helpers/find');
+const startExternal = require('./startExternal');
 
 module.exports = (context, search) => {
+  startExternal(context, 'imove-pg');
   findAndApply(context, search, (_, { name, path }) => {
     const cmd = `docker run -v ${path}:/app --network ${context.name} -e NODE_ENV=local --rm --name ${name}-setupdb ${name} npm run db-migration`;
 
