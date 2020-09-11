5753d6d91def65ad1ad30d23dafc44ef72ea2ef2
Jørgen Landsnes <jorgen.landsnes@miles.no>
2020-09-11 14:59:52 +0200

    fix: dev without args should print context

--- dev.js
+++ dev.js
@@ -4,7 +4,21 @@ const getArgs = require('./helpers/get_args');
 const operations = require('./operations');
 const { operation, options } = getArgs();
 
+const fetchContext = () => {
+  try {
+    const getContext = require('./helpers/get_context');
+    return getContext(operation, options);
+  } catch (err) {
+    console.log(err.message);
+    process.exit(4);
+  }
+}
+
 switch (operation) {
+  case undefined:
+  case '':
+    console.log(fetchContext());
+    break;
   case 'help':
     operations.help();
     break;
@@ -18,14 +32,7 @@ switch (operation) {
     break;
 
   default:
-    let context;
-    try {
-      const getContext = require('./helpers/get_context');
-      context = getContext(operation, options);
-    } catch (err) {
-      console.log(err.message);
-      process.exit(4);
-    }
+    const context = fetchContext();
     
     if (operation) {
       if (operations[operation]) {
