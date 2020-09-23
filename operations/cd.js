b5bac3705c39727cca7b43cc021d578b54ecdb11
Jørgen Landsnes <jorgen.landsnes@miles.no>
2020-09-23 13:31:55 +0200

    fix: Add explanation for the very clever cd function

--- operations/cd.js
+++ operations/cd.js
@@ -1,7 +1,23 @@
 const shell = require('shelljs');
+const chalk = require('chalk');
 const { findAndApply } = require('../helpers/find');
 
 module.exports = (context, search) => {
-  const match = findAndApply(context, search, (_, { path }) => path);
-  shell.echo(match || context.root);
+  if (search === '?' || search === '-h') {
+    shell.echo(`
+You cannot change the current directory from a script like the dev-cli you are using now. However, you can create a shell 
+function like this example for bash:`);
+
+    shell.echo(chalk.green(`
+function d(){
+  DIR=\`dev cd $1\`
+  echo $DIR
+  cd $DIR
+}
+`));
+    shell.echo(chalk`Put this in your ~/.bash_profile to enable {green d [repoName]} to quicky enter the matching folder.`);
+  } else {
+    const match = findAndApply(context, search, (_, { path }) => path);
+    shell.echo(match || context.root);
+  }
 };
