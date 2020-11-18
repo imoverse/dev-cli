296209b5821b8c7503ab2ce552dda87adb34cb84
Marit Hellestø <marit.hellesto@apparat.no>
2020-11-18 14:53:53 +0100

    updated create service with newer packages, and removed some bugs

--- templates/service/src/[resource]/routes.js
+++ templates/service/src/[resource]/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const log = require('@imoverse/logger');
-const authorize = require('@imoverse/authorize');
+const { authorize, setTenantId, permissionCheck } = require('@imoverse/authorize');
+const { findTenantId } = require('@imoverse/lib');
 const { curry } = require('ramda');
 const { validateInput } = require('@imoverse/validation');
 const {{primaryResourcePlural}} = require('./db');
@@ -22,26 +23,35 @@ const handleError = curry(
 );
 
 //TODO: Should getters not need authorization in your service?
-router.get('/', getTenantId, (req, res) =>
+
+router.get('/', findTenantId, (req, res) =>
 {{primaryResourcePlural}}.all(res.locals.tenantId)
     .then(cats => res.send(cats))
     .catch(handleError(res)));
 
-router.get('/:id', getTenantId, ({ params }, res) =>
-{{primaryResourcePlural}}.find(params.id, res.locals.tenantId)
+router.get('/:id', findTenantId, ({ params }, res) =>
+  (await {{primaryResourcePlural}}.find(params.id, res.locals.tenantId)).cata({
+  Failure: handleError(res),
+  Ok: {{primaryResourceSingular}} => {{primaryResourceSingular}}.cata(
+    () => res.sendStatus(404),
+    x => res.send(x),
+  )
+}));
     .then({{primaryResourceSingular}} => res.send({{primaryResourceSingular}}))
     .catch(handleError(res)));
 
 router.use(authorize());
+router.use(setTenantId);
+//TODO: add appropriate permissions router.use(permissionCheck(['read:<resource>', 'write:<resource']));
 
-router.post('/', mapBodyWithTenantId, validateInput(validateAdd{{primaryResourceSingularUcFirst}}), (_, res) =>
-  publish{{primaryResourceSingularUcFirst}}Created(res.locals.mappedBody)
+router.post('/', mapTenantId, validateInput(validateAdd{{primaryResourceSingularUcFirst}}), (_, res) =>
+  publish{{primaryResourceSingularUcFirst}}Created(res.locals.mappedBody, res.locals.tenantId)
     .then(() =>
       res.sendStatus(201))
     .catch(handleError(res)));
 
 router.put('/:id', validateInput(validateUpdate{{primaryResourceSingularUcFirst}}), ({ params, body }, res) =>
-  publish{{primaryResourceSingularUcFirst}}Updated({ id: params.id, ...body })
+  publish{{primaryResourceSingularUcFirst}}Updated({ id: params.id, ...body }, res.locals.tenantId)
     .then(() =>
       res.sendStatus(204))
     .catch(handleError(res)));
--- templates/service/src/index.js
+++ templates/service/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const events = require('@imoverse/events');
 const log = require('@imoverse/logger');
-const { InvalidInputError, corsWhitelist } = require('@imoverse/lib');
+const { handleRouteErrors, corsWhitelist } = require('@imoverse/lib');
 const { {{primaryResourcePlural}}Router, {{primaryResourcePlural}}Events: eventTypes, {{primaryResourcePlural}}Handlers: handlers } = require('./{{primaryResourcePlural}}');
 
 const app = express();
@@ -23,16 +23,7 @@ events
   .subscribe(eventTypes.{{primaryResourceSingularUc}}_DELETED, handlers.handle{{primaryResourceSingularUcFirst}}Deleted)
   .connect();
 
-app.use((err, req, res, next) => { // eslint-disable-line
-  log.error(err);
-  let status = 500;
-  let message = 'Internal server error';
-  if (err instanceof InvalidInputError) {
-    status = 400;
-    message = err.message;
-  }
-  res.status(status).send({ message, status });
-});
+app.use(handleRouteErrors);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => log.info(`{{projectName}} service running on port ${port}!`));
