341d6a2005328d320bd1e148e752d3b452382a1d
Øyvind <oyvind@imove.no>
2021-04-16 15:30:44 +0200

    WIP

--- helpers/run_container.js
+++ helpers/run_container.js
@@ -1,19 +1,33 @@
 const shell = require('shelljs');
 
-const getPortMapping = port => {
+const getPortMapping = (port, debugport = false) => {
+  const toPort = debugport ? 9229 : 3000;
   if (typeof port === 'number' || port.indexOf(':') === -1) {
-    return `${port}:3000`;
+    return `${port}:${toPort}`;
   }
   return port;
 };
 
+const getDebugParameter = port => port ? ` --inspect=0.0.0.0:${port}` : '';
+const getDebugPort = port => {
+  const add = 6000;
+  if (typeof port === 'number') {
+    return port + add;
+  }
+  const colonIndex = port.indexOf(':');
+  if (colonIndex >= 0) {
+    return parseInt(port.substr(0, colonIndex), 10) + add;
+  }
+  return port;
+};
 const runContainer = (context, containerConfig) => {
   const flags = ['-d'];
   let itr = 0;
   const {
     name,
     port,
-    cmd = 'npm run dev',
+    debugport = getDebugPort(port),
+    cmd = `npm run dev`,
     volume,
   } = containerConfig;
   let containerPorts;
@@ -23,6 +37,8 @@ const runContainer = (context, containerConfig) => {
   } else {
     containerPorts = `-p ${getPortMapping(port)}`;
   }
+
+  const debugports = `-p ${getPortMapping(debugport, true)}`;
 
   context.options.forEach(flag => {
     if (flag.startsWith('-')) { flags[itr += 1] = flag; } else flags[1] = flag;
@@ -41,7 +57,7 @@ const runContainer = (context, containerConfig) => {
   if (volume === false) {
     containerVolume = '';
   }
-  const run = `docker run -e NODE_ENV=local ${env} ${flags[0]} ${containerPorts} ${containerVolume} --network ${context.name} --name ${containerConfig.name} ${containerConfig.name} ${containerCmd}`;
+  const run = `docker run -e NODE_ENV=local ${env} ${flags[0]} ${containerPorts} ${debugports} ${containerVolume} --network ${context.name} --name ${containerConfig.name} ${containerConfig.name} ${containerCmd}`;
 
   shell.echo(run);
   shell.exec(run);
