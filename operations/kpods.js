b50ed344eb069b20934ebb58b7b34ed606a4e868
Jørgen Landsnes <jorgen.landsnes@miles.no>
2021-06-15 15:22:08 +0200

    feat: Pass options to kubectl

--- operations/kpods.js
+++ operations/kpods.js
@@ -1,7 +1,7 @@
 const shell = require('shelljs');
 
-module.exports = () => {
-  const cmd1 = 'kubectl get pods';
+module.exports = (context) => {
+  const cmd1 = `kubectl get pods ${context.options.join(' ')}`;
   shell.echo(cmd1);
   shell.exec(cmd1);
 };
